8633722a2908417f8e48a5db998bc1fcfc9d371d
GitHub321109 <162006427+GitHub321109@users.noreply.github.com>
2024-03-05 19:37:03 +0500

    2nd comit

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ let contact = document.querySelector("div#contact") ;
 function contactPage(){
     contact.style.display = "block" ;
 }
+
+function hideContact(){
+    contact.style.display = "none" ;
+}
